0e0268f2b5b1d2d56c1318313ac5b080046da47e
Rachel Wyatt <rw126@evansville.edu>
2018-11-10 16:58:22 +0000

    CLI inputs

--- lib/word_of_the_day/cli.rb
+++ lib/word_of_the_day/cli.rb
@@ -5,32 +5,35 @@ class WordOfTheDay::CLI
     menu
     goodbye
   end
-  
+
   def greeting
   puts "Welcome to Word Of The Day! The Word of the Day for November 9th is: Voracious
-    Voracious adj. An eager approach to an activity; Wanting or devouring great quantities of something or somebody: 'She has a voracious appetite for life.'"
+    Voracious adj. An eager approach to an activity; Wanting or devouring great quantities of something or somebody: 'She has a voracious appetite for life.'
+    1. Word 1
+    2. Word 2
+    3. Word 3"
 
   end
-  
+
  def menu
     input = nil
     while input != "exit"
       puts "Type the corresponding number to learn a word's definition, and exit to exit the CLI."
       input = gets.strip
-      
-      if input == "1."
+
+      if input.to_i == 1
         puts "Definition for Word 1"
-        elsif input == "2."
+      elsif input.to_i == 2
         puts "Definition for Word 2"
       else puts "Not sure what you meant, please type the number of the word you want to learn more about, list words, or exit."
-      end 
-    end 
+      end
+    end
   end
 
 
-  
+
   def goodbye
     puts "Come back tomorrow to learn a new word!"
   end
-  
+
 end
--- lib/word_of_the_day/word.rb
+++ lib/word_of_the_day/word.rb
@@ -1,4 +1,11 @@
-class WordOfTheDay::Word 
+class WordOfTheDay::Word
 
-  
+attr_accessor :definition, :type
+
+@@all = []
+
+def self.create_word
+
+end
+
 end
