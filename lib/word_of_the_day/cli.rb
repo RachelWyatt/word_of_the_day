a16c87e81176de0403a499309c739c11fd9b109d
Rachel Wyatt <rw126@evansville.edu>
2018-11-14 15:26:12 +0000

    cli readability improvements

--- lib/word_of_the_day/cli.rb
+++ lib/word_of_the_day/cli.rb
@@ -22,12 +22,12 @@ class WordOfTheDay::CLI
  def menu
     input = nil
     until input == "exit"
-      puts "Type the corresponding number to learn more about a word, list to see the lsit of words and exit to exit the CLI."
+      puts "Type the corresponding number to learn more about a word, list to see the list of words and exit to exit the CLI."
       input = gets.strip
 
       if input.to_i > 0 && input.to_i < 13
         word = @words[input.to_i-1]
-        puts "#{word.type} #{word.definition}"
+        puts "#{word.name} #{word.type} #{word.definition}"
       elsif input == 'list'
         list_words
       elsif input == "exit"
