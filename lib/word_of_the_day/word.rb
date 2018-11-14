01a817e78a38d9c603038fbe39947dc15b107cbc
Rachel Wyatt <rw126@evansville.edu>
2018-11-14 15:04:33 +0000

    fixed stray semi-colons

--- lib/word_of_the_day/word.rb
+++ lib/word_of_the_day/word.rb
@@ -13,8 +13,8 @@ end
 def self.create_word_list
   WordOfTheDay::Scraper.scrape_words.each do |word|
     entry = self.new
-    split_word = word.split("  adj. " || "  n. ")
-    entry.name = word.split.first
+    split_word = word.split
+    entry.name = word.match(/[A-Z].*\b\s{2}/)
     entry.definition = split_word[-1]
   end
   @@all
